38574a32dfd91063d28cad4747452c9bd180af49
Najeebullah Hussaini <najeebhussaini27@gmail.com>
2025-04-06 12:52:23 +0200

    Create semantic_routing.py

---
+++ routing/semantic_routing.py
@@ -0,0 +1,134 @@
+from langchain.utils.math import cosine_similarity
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+# Two prompts
+physics_template = """You are a very smart physics professor. \
+You are great at answering questions about physics in a concise and easy to understand manner. \
+When you don't know the answer to a question you admit that you don't know.
+
+Here is a question:
+{query}"""
+
+math_template = """You are a very good mathematician. You are great at answering math questions. \
+You are so good because you are able to break down hard problems into their component parts, \
+answer the component parts, and then put them together to answer the broader question.
+
+Here is a question:
+{query}"""
+
+# Embed prompts
+embeddings = OpenAIEmbeddings()
+prompt_templates = [physics_template, math_template]
+prompt_embeddings = embeddings.embed_documents(prompt_templates)
+
+# Route question to prompt 
+def prompt_router(input):
+    # Embed question
+    query_embedding = embeddings.embed_query(input["query"])
+    # Compute similarity
+    similarity = cosine_similarity([query_embedding], prompt_embeddings)[0]
+    most_similar = prompt_templates[similarity.argmax()]
+    # Chosen prompt 
+    print("Using MATH" if most_similar == math_template else "Using PHYSICS")
+    return PromptTemplate.from_template(most_similar)
+
+
+chain = (
+    {"query": RunnablePassthrough()}
+    | RunnableLambda(prompt_router)
+    | ChatOpenAI()
+    | StrOutputParser()
+)
+
+print(chain.invoke("What's a black hole"))
+from langchain_community.document_loaders import YoutubeLoader
+
+docs = YoutubeLoader.from_youtube_url(
+    "https://www.youtube.com/watch?v=pbAd8O1Lvm4", add_video_info=True
+).load()
+
+docs[0].metadata
+import datetime
+from typing import Literal, Optional, Tuple
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+class TutorialSearch(BaseModel):
+    """Search over a database of tutorial videos about a software library."""
+
+    content_search: str = Field(
+        ...,
+        description="Similarity search query applied to video transcripts.",
+    )
+    title_search: str = Field(
+        ...,
+        description=(
+            "Alternate version of the content search query to apply to video titles. "
+            "Should be succinct and only include key words that could be in a video "
+            "title."
+        ),
+    )
+    min_view_count: Optional[int] = Field(
+        None,
+        description="Minimum view count filter, inclusive. Only use if explicitly specified.",
+    )
+    max_view_count: Optional[int] = Field(
+        None,
+        description="Maximum view count filter, exclusive. Only use if explicitly specified.",
+    )
+    earliest_publish_date: Optional[datetime.date] = Field(
+        None,
+        description="Earliest publish date filter, inclusive. Only use if explicitly specified.",
+    )
+    latest_publish_date: Optional[datetime.date] = Field(
+        None,
+        description="Latest publish date filter, exclusive. Only use if explicitly specified.",
+    )
+    min_length_sec: Optional[int] = Field(
+        None,
+        description="Minimum video length in seconds, inclusive. Only use if explicitly specified.",
+    )
+    max_length_sec: Optional[int] = Field(
+        None,
+        description="Maximum video length in seconds, exclusive. Only use if explicitly specified.",
+    )
+
+    def pretty_print(self) -> None:
+        for field in self.__fields__:
+            if getattr(self, field) is not None and getattr(self, field) != getattr(
+                self.__fields__[field], "default", None
+            ):
+                print(f"{field}: {getattr(self, field)}")
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+system = """You are an expert at converting user questions into database queries. \
+You have access to a database of tutorial videos about a software library for building LLM-powered applications. \
+Given a question, return a database query optimized to retrieve the most relevant results.
+
+If there are acronyms or words you are not familiar with, do not try to rephrase them."""
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "{question}"),
+    ]
+)
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+structured_llm = llm.with_structured_output(TutorialSearch)
+query_analyzer = prompt | structured_llm
+
+query_analyzer.invoke({"question": "rag from scratch"}).pretty_print()
+query_analyzer.invoke(
+    {"question": "videos on chat langchain published in 2023"}
+).pretty_print()
+query_analyzer.invoke(
+    {"question": "videos that are focused on the topic of chat langchain that are published before 2024"}
+).pretty_print()
+query_analyzer.invoke(
+    {
+        "question": "how to use multi-modal models in an agent, only videos under 5 minutes"
+    }
+).pretty_print()
