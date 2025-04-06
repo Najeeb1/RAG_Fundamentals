7e3d9116ce10f68af227211a407f6826c43bc0fd
Najeebullah Hussaini <najeebhussaini27@gmail.com>
2025-04-06 12:22:54 +0200

    Create loader.py

---
+++ basics/loader.py
@@ -0,0 +1,11 @@
+import bs4
+from langchain_community.document_loaders import WebBaseLoader
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+blog_docs = loader.load()
