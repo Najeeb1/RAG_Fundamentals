657d0b5f402cc541cde0cb6244ee6f1229e1bc26
Najeebullah Hussaini <najeebhussaini27@gmail.com>
2025-04-06 12:38:40 +0200

    Update multi-query.py

--- query_transfromation/multi-query.py
+++ query_transfromation/multi-query.py
@@ -1,4 +1,8 @@
 from langchain.prompts import ChatPromptTemplate
+from langchain.load import dumps, loads
+from operator import itemgetter
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables import RunnablePassthrough
 
 # Multi Query: Different Perspectives
 template = """You are an AI language model assistant. Your task is to generate five 
@@ -17,3 +21,41 @@ generate_queries = (
     | StrOutputParser() 
     | (lambda x: x.split("\n"))
 )
+
+
+
+def get_unique_union(documents: list[list]):
+    """ Unique union of retrieved docs """
+    # Flatten list of lists, and convert each Document to string
+    flattened_docs = [dumps(doc) for sublist in documents for doc in sublist]
+    # Get unique documents
+    unique_docs = list(set(flattened_docs))
+    # Return
+    return [loads(doc) for doc in unique_docs]
+
+# Retrieve
+question = "What is task decomposition for LLM agents?"
+retrieval_chain = generate_queries | retriever.map() | get_unique_union
+docs = retrieval_chain.invoke({"question":question})
+
+# RAG
+template = """Answer the following question based on this context:
+
+{context}
+
+Question: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+llm = ChatOpenAI(temperature=0)
+
+final_rag_chain = (
+    {"context": retrieval_chain, 
+     "question": itemgetter("question")} 
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+final_rag_chain.invoke({"question":question})
