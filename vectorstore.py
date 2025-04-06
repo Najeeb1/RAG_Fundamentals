407bc0540337849d5064b8a4eb0edc5b58a7b398
Najeebullah Hussaini <najeebhussaini27@gmail.com>
2025-04-06 12:23:48 +0200

    Create vectorstore.py

---
+++ vectorstore.py
@@ -0,0 +1,6 @@
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+vectorstore = Chroma.from_documents(documents=splits, 
+                                    embedding=OpenAIEmbeddings())
+
+retriever = vectorstore.as_retriever()
