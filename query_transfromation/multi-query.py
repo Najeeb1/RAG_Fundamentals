fbdf250e3b7fa5a47a1f2525405d1254a98f829c
Najeebullah Hussaini <najeebhussaini27@gmail.com>
2025-04-06 12:36:52 +0200

    Create multi-query.py

---
+++ query_transfromation/multi-query.py
@@ -0,0 +1,19 @@
+from langchain.prompts import ChatPromptTemplate
+
+# Multi Query: Different Perspectives
+template = """You are an AI language model assistant. Your task is to generate five 
+different versions of the given user question to retrieve relevant documents from a vector 
+database. By generating multiple perspectives on the user question, your goal is to help
+the user overcome some of the limitations of the distance-based similarity search. 
+Provide these alternative questions separated by newlines. Original question: {question}"""
+prompt_perspectives = ChatPromptTemplate.from_template(template)
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+
+generate_queries = (
+    prompt_perspectives 
+    | ChatOpenAI(temperature=0) 
+    | StrOutputParser() 
+    | (lambda x: x.split("\n"))
+)
